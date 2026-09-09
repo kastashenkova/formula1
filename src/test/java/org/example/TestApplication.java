0d8f5cd215fe8c078bca6c66bbca7e42769907d9
kastashenkova <astashenkova.katya@gmail.com>
2026-09-10 00:17:05 +0300

    initialize project

---
+++ src/test/java/org/example/TestApplication.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(Application::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
