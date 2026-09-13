7855be2a3b664498156f0b276d79405fa2c34396
kastashenkova <astashenkova.katya@gmail.com>
2026-09-14 01:05:39 +0300

    add modules

---
+++ backend/worker-pool/src/main/java/example/WorkerPoolApplication.java
@@ -0,0 +1,13 @@
+package example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkerPoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkerPoolApplication.class, args);
+    }
+
+}
